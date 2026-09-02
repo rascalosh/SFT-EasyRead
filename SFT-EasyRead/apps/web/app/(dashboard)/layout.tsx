d45ee86af779019773abccbbb925bea6e360cc9c
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-09-02 19:34:28 +0700

    Delete useless file

--- SFT-EasyRead/apps/web/app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardShell from "../../ui/components/DashboardShell";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardShell>{children}</DashboardShell>;
-}
